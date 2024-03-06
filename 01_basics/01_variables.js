bfff1deec98939bc93357684c5554efd7ea27ce5
copiness <111597134+copiness@users.noreply.github.com>
2024-03-06 05:53:41 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "akl@gogole.com"
+var accountPassword = "12345"
+accountCity = "Odisha"
+
+//accountId = 2 //not allowed
+
+accountEmail = "kul@.com"
+accountPassword ="0987654"
+accountCity = "bbsr"
+
+console.log(accountId);
+
+/* not to use var */
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
